99f0985f50c662653b5afcb991740810d8c77975
devodev <abalexandrebarone@gmail.com>
2020-08-29 21:27:30 -0400

    remove logger return code logic. Use component to draw.

--- logger.go
+++ logger.go
@@ -16,27 +16,3 @@ func NewLogger() *SimpleLogger {
 
 	return &SimpleLogger{logger}
 }
-
-// ErrorType .
-type ErrorType int
-
-// ErrorType enum .
-const (
-	GLFWInitError ErrorType = iota + 1
-	GLFWCreateWindowError
-	GLInitError
-	GLCompileShaderrError
-)
-
-// FatalError logs.
-func (l *SimpleLogger) FatalError(e ErrorType, err error) int {
-	errorMessages := map[ErrorType]string{
-		GLFWInitError:         "could not initialize GLFW",
-		GLFWCreateWindowError: "could not create window",
-		GLInitError:           "could not initialize OpenGL",
-		GLCompileShaderrError: "could not compile shader",
-	}
-	l.Errorf(errorMessages[e]+": %s", err)
-
-	return int(e)
-}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"runtime"
 	"strings"
 
@@ -10,6 +9,26 @@ import (
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
+// shaders
+var (
+	vertexShaderSource = `
+        #version 410
+        in vec3 vp;
+        void main() {
+            gl_Position = vec4(vp, 1.0);
+        }
+    ` + "\x00"
+
+	fragmentShaderSource = `
+        #version 410
+        out vec4 frag_colour;
+        void main() {
+            frag_colour = vec4(1, 1, 1, 1);
+        }
+    ` + "\x00"
+)
+
+// window attributes
 var (
 	windowWidth  = 640
 	windowHeight = 480
@@ -23,107 +42,119 @@ func init() {
 }
 
 func main() {
-	os.Exit(mainWithReturnCode())
-}
-
-func mainWithReturnCode() int {
 	// Create logger
 	logger := NewLogger()
 
 	// initialize GLFW
-	if err := glfw.Init(); err != nil {
-		logger.FatalError(GLFWInitError, err)
+	window, err := initGLFW()
+	if err != nil {
+		logger.Error(err)
+		return
 	}
 	defer glfw.Terminate()
 
-	// log GLFW version
-	glfwVersion := glfw.GetVersionString()
-	logger.Printf("GLFW version: %s", glfwVersion)
+	logger.Printf("GLFW version: %s", glfw.GetVersionString())
 
-	// create a window
-	window, err := createWindow(windowWidth, windowHeight, windowTitle)
-	if err != nil {
-		logger.FatalError(GLFWCreateWindowError, err)
-	}
 	window.MakeContextCurrent()
 
 	// initialize OpenGL
 	// *always do this after a call to `window.MakeContextCurrent()`
-	if err := gl.Init(); err != nil {
-		return logger.FatalError(GLInitError, err)
+	program, err := initOpenGL()
+	if err != nil {
+		logger.Error(err)
+		return
 	}
 
-	// log OpenGL version
-	glVersion := gl.GoStr(gl.GetString(gl.VERSION))
-	logger.Printf("OpenGL version: %s", glVersion)
+	logger.Printf("OpenGL version: %s", gl.GoStr(gl.GetString(gl.VERSION)))
 
 	// create a triangle VAO
 	triangle := []float32{
+		// X, Y, Z
 		0, 0.5, 0, // top
 		-0.5, -0.5, 0, // left
 		0.5, -0.5, 0, // right
 	}
-	vao := makeVao(triangle)
 
-	// load shaders
-	vertexShaderSource := `
-        #version 410
-        in vec3 vp;
-        void main() {
-            gl_Position = vec4(vp, 1.0);
-        }
-    ` + "\x00"
-
-	fragmentShaderSource := `
-        #version 410
-        out vec4 frag_colour;
-        void main() {
-            frag_colour = vec4(1, 1, 1, 1);
-        }
-    ` + "\x00"
-
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		return logger.FatalError(GLCompileShaderrError, err)
-	}
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		return logger.FatalError(GLCompileShaderrError, err)
-	}
-
-	// create a program and attach our shaders
-	prog := gl.CreateProgram()
-	gl.AttachShader(prog, vertexShader)
-	gl.AttachShader(prog, fragmentShader)
-	gl.LinkProgram(prog)
+	var components []*Component
+	components = append(components, NewComponent(triangle))
 
 	// start event loop
 	for !window.ShouldClose() {
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		gl.UseProgram(prog)
-
-		gl.BindVertexArray(vao)
-		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
-
-		glfw.PollEvents()
-		window.SwapBuffers()
+		draw(components, window, program)
 	}
-
-	return 0
 }
 
-func createWindow(width, height int, title string) (*glfw.Window, error) {
+// Component .
+type Component struct {
+	points []float32
+	vao    uint32
+}
+
+// NewComponent .
+func NewComponent(points []float32) *Component {
+	return &Component{points: points, vao: makeVao(points)}
+}
+
+// Draw .
+func (c *Component) Draw() {
+	gl.BindVertexArray(c.vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(c.points)/3))
+}
+
+func draw(components []*Component, window *glfw.Window, program uint32) {
+	// clear old frame and register the program to use
+	// for subsequent draw calls
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	// actual drawing
+	for _, c := range components {
+		c.Draw()
+	}
+
+	// events and buffer swap
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+func initGLFW() (*glfw.Window, error) {
+	if err := glfw.Init(); err != nil {
+		return nil, fmt.Errorf("could not initialize GLFW: %s", err)
+	}
+
 	glfw.WindowHint(glfw.ContextVersionMajor, 4)
 	glfw.WindowHint(glfw.ContextVersionMinor, 6)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 	glfw.WindowHint(glfw.Resizable, glfw.False)
 
-	window, err := glfw.CreateWindow(width, height, title, nil, nil)
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, windowTitle, nil, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not create window: %s", err)
 	}
 	return window, nil
+}
+
+func initOpenGL() (uint32, error) {
+	if err := gl.Init(); err != nil {
+		return 0, fmt.Errorf("could not initialize OpenGL: %s", err)
+	}
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, fmt.Errorf("could not compile vertex shader: %s", err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, fmt.Errorf("could not compile fragment shader: %s", err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+
+	return prog, nil
 }
 
 func makeVao(points []float32) uint32 {
