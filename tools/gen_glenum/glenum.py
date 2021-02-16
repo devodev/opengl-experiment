b8383192368aaa609d081d654214645bb4e92152
devodev <abalexandrebarone@gmail.com>
2021-02-15 22:15:22 -0500

    update glenum script

--- tools/gen_glenum/glenum.py
+++ tools/gen_glenum/glenum.py
@@ -5,22 +5,25 @@ if __name__ == '__main__':
     d = dict()
     for line in sys.stdin:
         idx, text = line.split(':')
+        if 'TIMEOUT_IGNORED' in text:
+            continue
         d.setdefault(int(idx, 16), []).append(text.strip())
 
     sortedD = list(sorted(d.items()))
-    key_padding_size = len(str(sortedD[len(sortedD)-1][0]))
+    key_padding_size = len(str(sortedD[len(sortedD)-1][0])) + 1
 
-    comment = ''
-    print('package opengl')
-    print()
-    print('// GlEnum string lookup')
-    print('var (')
-    print('    GlEnums = map[int][]string{')
+    output = list()
+    output.append('package opengl')
+    output.append('')
+    output.append('// GlEnum string lookup')
+    output.append('var (')
+    output.append('    GlEnums = map[uint32][]string{')
     for key, value in sortedD:
-        print('        ', end='')
-        if 'TIMEOUT_IGNORED' in value:
-            print('// ', end='')
-        print('{:<{}} []string{{{}}},'.format('{}:'.format(key), key_padding_size+1, ', '.join(map(lambda x: '"{}"'.format(x), value))))
-    print('    }')
-    print(')')
-    print()
+        map_key = '{}:'.format(key)
+        map_value = ', '.join(map(lambda x: '"{}"'.format(x), value))
+        output.append('        {:<{}} {{{}}},'.format(map_key, key_padding_size, map_value))
+    output.append('    }')
+    output.append(')')
+    output.append('')
+
+    print('\n'.join(output), end='')
